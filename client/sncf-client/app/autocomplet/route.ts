b9bc7fdad9592a9dbbc97d090a3eb3b6535818d4
Rasaboun <rayane.amie@gmail.com>
2023-08-24 00:24:18 +0200

    fix: autocomplete server function error handling

--- client/sncf-client/app/autocomplet/route.ts
+++ client/sncf-client/app/autocomplet/route.ts
@@ -1,23 +1,25 @@
-
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
 type places = {
-	id: string,
-	name: string,
-	embedded_type: string
-}
+  id: string;
+  name: string;
+  embedded_type: string;
+};
 
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
-  const header = { 'Authorization': process.env.navitia || 'non' };
+  const header = { Authorization: process.env.navitia || "non" };
+  let data = null;
   const res = await fetch(
     `https://api.navitia.io/v1/coverage/fr-idf/places?q=${q}`,
     {
-      headers: new Headers(header)
+      headers: new Headers(header),
     }
   );
-
-  const data = await res.json()
-  return NextResponse.json(data.places)
+  if (res.ok) {
+    data = await res.json();
+    if (data) return NextResponse.json(data.places);
+  } else
+    return NextResponse.json({ message: "Sorry fail to connect to SNCF API" });
 }
